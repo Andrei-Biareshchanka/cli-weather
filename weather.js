aa448a747591d7e0231b33779d7590b0f0a519e7
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-06-27 16:17:33 +0200

    feat: add logic of save sity and show weather

--- weather.js
+++ weather.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node - this file run by node
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { printHelp, printSuccess, printWeather, printError } from './services/log.service.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getIcon } from './services/api.service.js';
 
 const saveToken = async (token) => {
   if (!token.length) {
@@ -11,26 +12,57 @@ const saveToken = async (token) => {
   }
   try {
     await saveKeyValue(TOKEN_DICTIONARY.token, token);
-    printSuccess('Token saved')
+    printSuccess('Token saved');
   } catch (error) {
     printError(error);
   }
 }
 
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Citi is empty');
+    return;
+  }
+
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('City saved');
+  } catch (error) {
+    printError(error);
+  }
+}
+
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      printError('City is incorrect');
+    } else if (error?.response?.status == 401) {
+      printError('Token is incorrect');
+    } else {
+      printError(error.message);
+    }
+  }
+
+}
+
 const initCLI = () => {
   const args = getArgs(process.argv);
 
-  if (args.h) { // enter help
-    printHelp();
+  if (args.h) {
+    return printHelp();
   }
-  if (args.s) { // save city
+  if (args.s) {
+    return saveCity(args.s);
   }
-  if (args.t) { // save token
+  if (args.t) {
     return saveToken(args.t)
   }
 
-  getWeather('warsaw');
-  // enter weather
+  return getForcast();
 }
 
 initCLI();
