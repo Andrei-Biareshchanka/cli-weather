d498768ddef3c88b1c74143f0e79472ddc23a489
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-06-27 13:33:55 +0200

    feat: add env variable like default parameter

--- services/api.service.js
+++ services/api.service.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 export const getWeather = async (city) => {
-  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 
   if (!token) {
     throw new Error('Api key is empty, set it use comand -t [API_KEY]')
